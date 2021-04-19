9ddf9256b12d68c4135af575fa8392ed61d82459
liefwerk <natjageni@gmail.com>
2021-04-19 23:52:29 +0200

    added new signup view

---
+++ src/views/SignUp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="sign-up-page">
+    <field>
+      <label>Email</label>
+      <input type="email" v-model="email">
+    </field>
+    <div>{{email}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>

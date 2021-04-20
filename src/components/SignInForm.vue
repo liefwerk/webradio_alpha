69be5b01d3867ea7ef4eb586e7b03d89cb309328
liefwerk <natjageni@gmail.com>
2021-04-20 22:15:09 +0200

    added new form

---
+++ src/components/SignInForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="sign-in-page main-form">
+    <form class="form" v-on:submit.prevent="signUp">
+      <fieldset class="email">
+        <legend>Email</legend>
+        <input type="email" v-model="email">
+      </fieldset>
+      <fieldset class="password">
+        <legend>Password</legend>
+        <input type="password" v-model="password">
+      </fieldset>
+      <button class="button" @click="signUp">Sign in</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { createClient } from '@supabase/supabase-js'
+export default {
+  name: 'SignInForm',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    signUp: async function () {
+      try {
+        const supabaseUrl = 'https://epqrpjmozlcsvbgkxjkp.supabase.co'
+        const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTE0ODMyNCwiZXhwIjoxOTMwNzI0MzI0fQ.GxLEzrl9Faolqb12sImfJ2OGGIGsYU72FYPJcrA0cO4'
+        const supabase = createClient(supabaseUrl, supabaseKey)
+
+        const { user, error } = await supabase.auth.signIn({
+          email: this.email,
+          password: this.password
+        })
+
+        if (error) console.log(error)
+        else {
+          console.log(user)
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,66 +1,57 @@
 <template>
-  <div class="container wrapper">
-    <div class="sign-up-page">
-      <form class="form" v-on:submit.prevent="signUp">
-        <fieldset class="email">
-          <legend>Email</legend>
-          <input type="email" v-model="email">
-        </fieldset>
-        <fieldset class="password">
-          <legend>Password</legend>
-          <input type="password" v-model="password">
-        </fieldset>
-        <button class="button" @click="signUp">Sign Up</button>
-      </form>
+  <div class="container">
+    <button @click="handleSwitchForm">I already have an account.</button>
+    <div class="wrapper" v-if="handleForm">
+      <SignUpForm />
+    </div>
+    <div class="wrapper" v-else>
+      <SignInForm />
     </div>
   </div>
 </template>
 
 <script>
-import { createClient } from '@supabase/supabase-js'
+import SignUpForm from '@/components/SignUpForm'
+import SignInForm from '@/components/SignInForm'
 
 export default {
   name: 'SignUp',
+  components: {
+    SignUpForm,
+    SignInForm
+  },
   data () {
     return {
-      email: '',
-      password: ''
+      handleForm: true
     }
   },
   methods: {
-    signUp: async function () {
-      try {
-        const supabaseUrl = 'https://epqrpjmozlcsvbgkxjkp.supabase.co'
-        const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTE0ODMyNCwiZXhwIjoxOTMwNzI0MzI0fQ.GxLEzrl9Faolqb12sImfJ2OGGIGsYU72FYPJcrA0cO4'
-        const supabase = createClient(supabaseUrl, supabaseKey)
-
-        const { user, error } = await supabase.auth.signUp({
-          email: this.email,
-          password: this.password
-        })
-
-        if (error) console.log(error)
-        else {
-          console.log(user)
-        }
-      } catch (err) {
-        console.log(err)
-      }
+    handleSwitchForm: function () {
+      console.log('switch!')
+      this.handleForm = !this.handleForm
     }
   }
 }
 </script>
 
 <style lang="scss">
-.container.wrapper {
+.container .wrapper {
   height: 100vh;
   position: absolute;
   top: 0;
   width: 100%;
   z-index: 0;
+
 }
 
-.sign-up-page {
+button {
+  position: relative;
+  z-index: 2
+}
+
+.sign-up-page{}
+
+.main-form {
   top: 50%;
   transform: translateY(-50%);
   position: relative;

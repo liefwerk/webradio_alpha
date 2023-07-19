33668124b61549c711de7be38530d1edc0d5c9f8
liefwerk <natjageni@gmail.com>
2023-07-19 15:19:02 +0200

    added vps url

--- alfonz-front/src/utils/apiUtils.js
+++ alfonz-front/src/utils/apiUtils.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://127.0.0.1:5000'
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5000' : 'http://play.natjs.fr'
 
 const headers = {
 	'Content-Type': 'application/json'

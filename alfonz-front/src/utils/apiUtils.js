f44537ccf454b45fa6b5b74e0cc4b30e112e35ba
liefwerk <natjageni@gmail.com>
2023-07-19 15:25:53 +0200

    test frontend cd

--- alfonz-front/src/utils/apiUtils.js
+++ alfonz-front/src/utils/apiUtils.js
@@ -1,4 +1,4 @@
-const baseURL = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5000' : 'http://play.natjs.fr';
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5000' : 'http://play.natjs.fr'
 
 const headers = {
 	'Content-Type': 'application/json'
